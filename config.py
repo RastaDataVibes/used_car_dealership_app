07aef92a9224a6af1722620680e6c5de26663819
Opio Bethle <opiobethle@gmail.com>
2025-11-18 00:27:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://zaga_admin:Y1hfxNhumANyST9BcyjxO0RC4m2KXOA2@dpg-d3n1av49c44c73cgjts0-a.oregon-postgres.render.com:5432/used_car_dealership_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
